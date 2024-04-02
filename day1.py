7fc8929fbf120564b89050dc0fc4d5e98c30178d
JEEVA E I <jeeva.in01@gmail.com>
2024-04-02 19:06:54 +0530

    day 1 added

---
+++ day1.py
@@ -0,0 +1,13 @@
+print("hello world\nhello world\n")
+#String concatenation
+
+print("hello"+"world")
+print("programming "+"language")
+print("python"+" "+"program")
+
+#Day 1 project- Band name generator
+
+print("Welcome to the Band Name Generator.")
+city_name=input("What's the name of the city you grew up in.?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be undefined "+ city_name +" "+pet_name )
