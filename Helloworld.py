48f7d254cc1ed99d7d5ad702a5aa8cbfe9d97ece
JEEVA E I <jeeva.in01@gmail.com>
2024-07-22 18:58:15 +0530

    changed

--- Helloworld.py
+++ Helloworld.py
@@ -4,4 +4,6 @@ print("hello world\nhello world\n")
 print("hello"+"world")
 print("programming "+"language")
 print("python"+" "+"program")
-
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
