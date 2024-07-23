e24a74b844eeede11844c44f2a16d519a7c8b942
JEEVA E I <jeeva.in01@gmail.com>
2024-07-23 19:47:57 +0530

    changes added

--- Day1-100_projects/Day4.py
+++ Day1-100_projects/Day4.py
@@ -1,8 +1,64 @@
-# heads or tails program
-import random
-value = random.randint(0,1)
-if(value == 1):
-  print("Heads")
-else:
-  print("Tails")
+#ROCK PAPER SCISSORS PROGRAM
+import random as rd
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+user_input = int(input("Type 0 for Rock, 1 for Paper or 2 for Scissors"))
+computer_input = rd.randint(0,2)
+print("YOUR CHOICE")
+if user_input == 0:
+  print(rock)
+elif user_input == 1:1
+  print(paper)
+else:
+  print(scissors) 
+
+print("COMPUTER CHOICE")
+if computer_input == 0:
+  print(rock)
+elif computer_input == 1:
+  print(paper)
+else:
+  print(scissors) 
+
+if user_input == computer_input:
+  print("The game end in Draw")
+elif user_input == 0:
+  if computer_input == 2:
+    print("You won")
+  else:
+    print("You lose")
+elif user_input == 1:
+  if computer_input ==0:
+    print("You Won")
+  else:
+    print("You Lose")
+else:
+  if user_input == 2 and computer_input == 1:
+    print("You Won")
+  else:
+    print("You Lose")
---
+++ Head_or_Tails.py
@@ -0,0 +1,7 @@
+# heads or tails program
+import random
+value = random.randint(0,1)
+if(value == 1):
+  print("Heads")
+else:
+  print("Tails")
