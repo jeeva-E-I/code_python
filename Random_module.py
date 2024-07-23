8fc2ceb0b83d165c2a1d34264527d91d3bc942f7
JEEVA E I <jeeva.in01@gmail.com>
2024-07-23 17:03:57 +0530

    altered

--- Random_module.py
+++ Random_module.py
@@ -1,6 +1,11 @@
 #Random module
 import random # A random module is used to generate random numbers
 
-random_num = random.randint(0,1) # it will generate any random number between 1 to 10  
-random_float = random.random() # it will create random number between 1 to 4.9 not 5
+random_num = random.randint(1,10) # it will generate any random number between 1 to 10  
+random_float = random.random()
 print(random_num)
+print(random_float)
+random_range = random.randrange(0,10,2)
+print(random_range)
+ran_float = random.random() * 10 # it will create random number between 1 to 9.99999 not 10.0
+print(ran_float)
