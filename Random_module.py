6b069f703c6aad093eb1ab9f4815e5a6441f74a8
JEEVA E I <jeeva.in01@gmail.com>
2024-07-27 11:39:03 +0530

    added choice method

--- Random_module.py
+++ Random_module.py
@@ -9,3 +9,6 @@ random_range = random.randrange(0,10,2)
 print(random_range)
 ran_float = random.random() * 10 # it will create random number between 1 to 9.99999 not 10.0
 print(ran_float)
+list1 = [1,2,3,4,5,6,7,8,9] 
+random_element = random.choice(list)    # it print the random element from the list
+print(random_element)
