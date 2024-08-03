b43e78a173a16b9ea4d3f6ea23f8a2fa93664eb7
JEEVA E I <jeeva.in01@gmail.com>
2024-08-03 11:51:16 +0530

    comment line added

--- Day1-100_projects/Day7.py
+++ Day1-100_projects/Day7.py
@@ -1,12 +1,13 @@
+#Hangman program
 import random
 import hangman_words
 import hangman_arts
 
                  
 print(hangman_arts.logo)
-chosen_word = random.choice(hangman_words.word_list)
+chosen_word = random.choice(hangman_words.word_list) # Get a random word from the wordlist
 display = []
-for item in range(0, len(chosen_word)):
+for item in range(0, len(chosen_word)): 
     display.append("_")
 lives = 6
 
@@ -16,15 +17,15 @@ while "_" in display:
         print("You have already entered the letter")
         guess = input("Enter different letter:").lower()
     for letter in chosen_word:
-        if letter == guess:
-            index = chosen_word.index(letter)
-            display[index] = guess
+        if letter == guess: # check whether the given guess letter is in given word
+            index = chosen_word.index(letter) 
+            display[index] = guess # change the letter
             print(display)
         
     if "_" not in display:
         print("YOU WON")
     else:
-        print(hangman_arts.stages[lives])
+        print(hangman_arts.stages[lives]) 
         print("You have entered the wrong text")
         lives -= 1
         print(f"{lives} chances are left")
