eaa121af66067666244779714fca5939fac25da8
JEEVA E I <jeeva.in01@gmail.com>
2024-08-03 11:24:55 +0530

    Added comment line

--- Day1-100_projects/Day9.py
+++ Day1-100_projects/Day9.py
@@ -21,20 +21,21 @@ print(logo)
 print("Welcome to the Secret auction program")
 dict ={}
 def input_dict():
+    """Get the input for the bidders"""
     name = input("Enter your name:")
     bid = input("What's your bid: $")
-    dict[name] = bid
+    dict[name] = bid # stores the user name and bid value in a dictionary
     x = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
     if x =='yes':
         clear()
-        input_dict()
+        input_dict() # Recurssively call the function to add another user name and bid value
     return dict
-auction_dict = input_dict()
+auction_dict = input_dict() # Tigger the function call
 print(auction_dict)
 value = 0
 key = ""
 
-for y,x in auction_dict.items():
+for y,x in auction_dict.items(): # Using for loop check the items in the dict to get the higher value in the auction
     x = int(x)
     if x > value:
         value = x
