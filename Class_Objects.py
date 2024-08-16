acc88373ef8c8e552b685c573c2f0f6105266c8f
JEEVA E I <jeeva.in01@gmail.com>
2024-08-16 18:40:16 +0530

    added content

--- Class_Objects.py
+++ Class_Objects.py
@@ -1,4 +1,7 @@
 #CLASS AND OBJECTS
+
+class User:
+  pass
 class Customer:
   bank_name = 'HDFC Bank' #Class variable
   
@@ -14,3 +17,8 @@ class Customer:
 c1 = Customer("John",19,6000) 
 c1.deposit(450)
 print(c1.bank_name)
+
+user = User() # Object creation
+user.id = 1 # Assigning attributes "{id}" is the attribute
+user.user_name = "Jeeva"
+print(user.user_name)
