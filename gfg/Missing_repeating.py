50eed0e6362a2e342c7584f728ccac7b71fc06be
jeeva-E-I <jeeva.in01@gmail.com>
2024-12-01 10:59:28 +0530

    code modified

--- gfg/Missing_repeating.py
+++ gfg/Missing_repeating.py
@@ -1,13 +1,22 @@
+"""Given an unsorted array arr of positive integers. One number a from the set [1, 2,....,n] is missing and one number b occurs twice in the array. 
+    Find numbers a and b."""
 def missing(arr):
-        result = 0
-        arr.sort()
-        new_set = set(arr)
-        positive = 1
-        while positive in new_set:
-            if arr.count(positive) >1:
-                result = arr[positive]
+    count_dict = {}
+    result = 0
+    for i in arr:
+        if i in count_dict:
+            count_dict[i] +=1
+        else:
+            count_dict[i] = 1
+    arr = set(arr)
+    positive =1
+    for i in arr:
+        if positive in arr:
             positive +=1
-        return [result,positive]
+        if count_dict[i] > 1:
+            result = i
+    return [result,positive]
+
         
-arr = [7, 2, 3, 4, 5, 6, 6]
+arr = [1, 2, 3, 4]
 print(missing(arr))
