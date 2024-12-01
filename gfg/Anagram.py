8ed6f3886c27d1f873e1ddc15460abbdff7342e5
jeeva-E-I <jeeva.in01@gmail.com>
2024-12-01 10:08:05 +0530

    Optimised code

--- gfg/Anagram.py
+++ gfg/Anagram.py
@@ -1,18 +1,7 @@
-s1 = "allergy"
-s2 ="allergic"
+s1 = "enag"
+s2 ="gane"
 def anagram(s1,s2):
-    flag ='false'
-    new_s1 = s1.lower()
-    new_s2 = s2.lower()
-    if len(new_s1) == len(new_s2):
-        for i in new_s1:
-            if i in new_s2:
-                flag = 'true'
-            else:
-                return 'false'
-        return flag
-    else:
-        return flag
+    return sorted(s1) == sorted(s2)
 
 print(anagram(s1,s2))
 
